610f55d8b8d31b6027999a47876061d80c12a024
Mateusz Tadla <mat@scalepoint.com>
2022-11-19 12:43:03 +0100

    start form test

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class FormTest extends TestBase{
@@ -5,5 +7,13 @@ public class FormTest extends TestBase{
     @Test
     public void shouldFillFormWithSuccess(){
         driver.get("http://51.75.61.161:9102/form.php");
+
+        WebElement firstNameInput = driver.findElement(By.id("inputFirstName3"));
+        firstNameInput.sendKeys("Jan");
+
+        driver.findElement(By.id("inputLastName3")).sendKeys("Kowalski");
+
     }
+
+
 }
