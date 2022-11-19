4d9d4e34b86792d6ef9cfc46601a5827869438c7
Mateusz Tadla <mat@scalepoint.com>
2022-11-19 10:55:03 +0100

    add ex1

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,6 +3,18 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+/*
+Utwórz metody add(int a, int b) oraz multiply(int a, int b). Pierwsza metoda powinna zwrócić wynik dodawania parametrów, a druga powinna zwracać zawszę liczbę 4.
+Stwórz test, przekaż w nim do metody add() liczby 2 oraz 3, następnie przy pomocy asercji sprawdź czy zwrócona wartość jest równa 5.
+Stwórz test, przekaż w nim do metody multiply() liczby 2 oraz 3, następnie przy pomocy asercji sprawdź czy zwrócona wartość jest równa 6.
+Stwórz dwie metody zgodnie z poniższy wymaganiami dla każdej co do posiadanych adnotacji oraz wyświetlanego tekstu przy pomocy System.out.Println():
+	metoda 1: @BeforeMethod, wyświetlenie „run before each test”
+	metoda 2: @AfterMethod, wyświetlenie „run after each test”
+Uruchom testy i przeanalizuj w jaki sposób zostały wyświetlone informacje z punktów 2-4.
+
+ */
+
+
 public class MyFirstTest {
 
     @BeforeMethod
