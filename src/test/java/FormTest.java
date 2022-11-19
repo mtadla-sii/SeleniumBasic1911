545c840bee24ac0c5d474ef04f1a3fc8750b85aa
Mateusz Tadla <mat@scalepoint.com>
2022-11-19 13:56:40 +0100

    random element list

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Random;
 
 
 public class FormTest extends TestBase{
@@ -18,11 +19,25 @@ public class FormTest extends TestBase{
         driver.findElement(By.id("inputEmail3")).sendKeys("j.k@w.pl");
         driver.findElement(By.id("inputAge3")).sendKeys("35");
 
-
+//        opcja w 1 linijce
 //        driver.findElements(By.name("gridRadiosSex")).get(0).click();
-        List<WebElement> allGenders = driver.findElements(By.name("gridRadiosSex"));
-        allGenders.get(0).click();
 
+//        opcja w 2 linijkach
+//        List<WebElement> allGenders = driver.findElements(By.name("gridRadiosSex"));
+//        allGenders.get(0).click();
+
+//        opcja losująca element z listy w 1 linijce
+//        getRandomElement(driver.findElements(By.name("gridRadiosSex"))).click();
+
+//        opcja losująca element z listy w 2 linijkach
+        List<WebElement> allGenders = driver.findElements(By.name("gridRadiosSex"));
+        getRandomElement(allGenders).click();
+    }
+
+    public WebElement getRandomElement(List<WebElement> elements){
+        Random random = new Random();
+        int randomIndex = random.nextInt(elements.size());
+        return elements.get(randomIndex);
     }
 
 }
