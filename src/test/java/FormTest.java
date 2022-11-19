41acaaa6509004c1dedb11eb712bab99cb10c22e
Mateusz Tadla <mat@scalepoint.com>
2022-11-19 13:49:49 +0100

    add list support

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+
 public class FormTest extends TestBase{
 
     @Test
@@ -12,8 +15,14 @@ public class FormTest extends TestBase{
         firstNameInput.sendKeys("Jan");
 
         driver.findElement(By.id("inputLastName3")).sendKeys("Kowalski");
+        driver.findElement(By.id("inputEmail3")).sendKeys("j.k@w.pl");
+        driver.findElement(By.id("inputAge3")).sendKeys("35");
+
+
+//        driver.findElements(By.name("gridRadiosSex")).get(0).click();
+        List<WebElement> allGenders = driver.findElements(By.name("gridRadiosSex"));
+        allGenders.get(0).click();
 
     }
-
 
 }
